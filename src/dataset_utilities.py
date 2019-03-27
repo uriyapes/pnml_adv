BUG FIX - dataset_utilities: in MnistAdversarial casting -1 to uint8 makes it 255. This causes the sign to be incorrect. Solution: operations should be done in float32 and later cast the result (after clipping) to uint8. train_utilities - supports adversarial training. NOTICE: there is a  bug in line 149 - the pixel range isn't 1 and isn't multiplied by epsilon. 1. adversarial_utilities - create_adversarial_mnist_sign_dataset now works on batches. 2. TrainClass now takes epsilon as input for adversarial training.

src/dataset_utilities.py
@@ -397,12 +397,16 @@ class MnistAdversarial(datasets.MNIST):
         super(MnistAdversarial, self).__init__(**kwargs)
         self.adversarial_sign_dataset_path = adversarial_sign_dataset_path
         self.epsilon = epsilon
+        self.test_data = self.test_data.type(torch.float32) #avoid underflow
         grp_size = 10
         for index in range(int(self.test_data.shape[0]/grp_size)):
             sign = np.load(os.path.join(self.adversarial_sign_dataset_path, str(index) + '.npy'))
             # sign = np.transpose(sign, (1, 2, 0)) #This is needed in cifar10 where we have 3 dimension
-            sign = torch.from_numpy(sign).type(torch.uint8)
+            sign = torch.from_numpy(sign)
+            sign = sign.type(torch.float32)
             self.test_data[index*grp_size:(index+1)*grp_size] = np.clip(self.test_data[index*grp_size:(index+1)*grp_size] + (epsilon * 255) * sign, 0, 255)
+        self.test_data = self.test_data.type(torch.uint8)
+        assert(self.test_data.min() >= 0)
 
 def create_adversarial_mnist_dataloaders(data_dir: str = './data', adversarial_dir: str = os.path.join('data', 'mnist_adversarial_sign'),
                                            epsilon: float = 0.5, batch_size: int = 128, num_workers: int = 4):

src/train_utilities.py
@@ -19,7 +19,7 @@ class TrainClass:
     """
 
     def __init__(self, params_to_train, learning_rate: float, momentum: float, step_size: list, gamma: float,
-                 weight_decay: float, logger=None, adv_learn_eps=0):
+                 weight_decay: float, logger=None, adv_learn_alpha=0, adv_learn_eps=0.05):
         """
         Initialize train class object.
         :param params_to_train: the parameters of pytorch Module that will be trained.
@@ -29,7 +29,7 @@ class TrainClass:
         :param gamma:  reducing the learning rate by gamma each step_size.
         :param weight_decay: L2 regularization.
         :param logger: logger class in order to print logs and save results.
-        :param adv_learn_eps: The weight that should be given to the adversarial learning regulaizer (0 means none).
+        :param adv_learn_alpha: The weight that should be given to the adversarial learning regulaizer (0 means none).
         """
 
         self.num_epochs = 20
@@ -48,6 +48,7 @@ class TrainClass:
                                                         milestones=step_size,
                                                         gamma=gamma)
         self.freeze_batch_norm = True
+        self.adv_learn_alpha = adv_learn_alpha
         self.adv_learn_eps = adv_learn_eps
 
     def train_model(self, model, dataloaders, num_epochs: int = 10, acc_goal=None):
@@ -108,8 +109,8 @@ class TrainClass:
         model.train()
 
         # Turn off batch normalization update
-        if self.freeze_batch_norm is True:
-            model = model.apply(set_bn_eval)
+        if self.freeze_batch_norm is True: # TODO: this seems to always work, meaning that batchnorm and dropout are irrelevant
+            model = model.apply(set_bn_eval) # this fucntion calls model.eval() which only effects dropout and batchnorm which will work in eval mode.
 
         train_loss = 0
         correct = 0
@@ -118,15 +119,8 @@ class TrainClass:
             # Adjust to CUDA
             images = images.cuda() if torch.cuda.is_available() else images
             labels = labels.cuda() if torch.cuda.is_available() else labels
-            if self.adv_learn_eps is not 0:
+            if self.adv_learn_alpha is not 0:
                 images.requires_grad = True
-                # adv_images = images.clone()
-                # adv_images.requires_grad = True
-                # y = y.cuda() if torch.cuda.is_available() else y
-                # x = images.detach()
-                # x = x.cuda() if torch.cuda.is_available() else x
-                # x.requires_grad = True
-                # xx = torch.autograd.Variable(images.data.clone(), requires_grad=True)
 
             # Forward
             self.optimizer.zero_grad()
@@ -138,7 +132,7 @@ class TrainClass:
 
             # Back-propagation
 
-            if self.adv_learn_eps is 0:
+            if self.adv_learn_alpha is 0:
                 loss.backward()
             else:
                 # The loss is averaged over the minibatch, this doesn't matter at all since the loss magnitude
@@ -146,13 +140,14 @@ class TrainClass:
                 # it's gradient magnitude.
                 loss.backward(retain_graph=True)
                 img_grad = images.grad.data
-                img_grad_eps = img_grad.sign() * 0.05 # TODO: change to eps
+                img_grad_eps = img_grad.sign() * self.adv_learn_eps
                 adv_images = images.data + img_grad_eps
                 torch.clamp(adv_images, 0, 1)
                 self.optimizer.zero_grad()
                 outputs = model(adv_images)
                 adv_loss = self.criterion(outputs, labels)  # Negative log-loss
-                total_loss = (1 - self.adv_learn_eps) * loss + self.adv_learn_eps * adv_loss
+                total_loss = (1 - self.adv_learn_alpha) * loss + self.adv_learn_alpha * adv_loss
+                images.requires_grad = False
                 total_loss.backward()
 
             self.optimizer.step()